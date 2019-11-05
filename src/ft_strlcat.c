daa1399eae9eea55cf302fb32e5e5b10bfb1f639
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-05 15:49:29 +0100

    ft_strlcat src

---
+++ src/ft_strlcat.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/04 18:51:00 by gbudau            #+#    #+#             */
+/*   Updated: 2019/11/05 15:47:39 by gbudau           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+static size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t			ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t i;
+	size_t j;
+	size_t k;
+	size_t l;
+
+	i = ft_strlen(dst) + ft_strlen(src);
+	j = 0;
+	k = ft_strlen(dst);
+	l = ft_strlen(dst);
+	if (!(dstsize))
+		return (i);
+	while (src[j] != '\0' && j + l < dstsize - 1)
+	{
+		dst[k] = src[j];
+		k++;
+		j++;
+	}
+	dst[k] = '\0';
+	return (i);
+}
