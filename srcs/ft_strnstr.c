97696084919a31d2523e3e2f4e16bef54193e0ee
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-09 20:27:33 +0100

    ft_strnstr src

---
+++ srcs/ft_strnstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/09 18:08:47 by gbudau            #+#    #+#             */
+/*   Updated: 2019/11/09 20:25:47 by gbudau           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	const char *retptr;
+	const char *reset;
+
+	if (!(*needle))
+		return ((char *)haystack);
+	while (len + 1 && *haystack)
+	{
+		if (*haystack == *needle && len)
+		{
+			reset = needle;
+			retptr = haystack;
+			while (*haystack == *needle && *needle && len)
+			{
+				haystack++;
+				needle++;
+				len--;
+			}
+			if (!(*needle))
+				return ((char *)retptr);
+			needle = reset;
+		}
+		haystack++;
+		len--;
+	}
+	return (NULL);
+}
