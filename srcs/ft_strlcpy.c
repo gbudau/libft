8d72550f173bfa078202234b9bd3d6952b653d8f
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-13 00:18:06 +0100

    Fixed some ft_strlcpy errors, not protected

--- srcs/ft_strlcpy.c
+++ srcs/ft_strlcpy.c
@@ -6,29 +6,19 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 14:39:35 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/11 00:06:17 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/11/13 00:04:08 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-static size_t	ft_strlentmp(const char *str)
-{
-	size_t i;
-
-	i = 0;
-	while (*(str + i))
-		i++;
-	return (i);
-}
-
-size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t len;
 	size_t i;
 
-	len = ft_strlentmp(src);
-	if (!(dstsize))
+	len = ft_strlen(src);
+	if (!dstsize)
 		return (len);
 	i = 0;
 	while (*(src + i) && i < dstsize - 1)
