9806e95c9cf7d1a1ab33029c2789693bc8182eb0
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-16 22:27:28 +0100

    ft_atoi src

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 02:04:46 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/15 17:53:05 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/11/16 22:08:09 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 
 int	ft_atoi(const char *str)
 {
-	int				r;
-	const char	*s;
+	unsigned long long int	nbr;
+	const char				*s;
+	int						pon;
+	int						i;
 
 	s = str;
-	while (*s)
-		while (ft_isspace(*s)
-				s++;
-		if (*s == "+" || *s == "-")
+	while (ft_isspace(*s))
 		s++;
-		while (ft_isdigit(*s))
-		s++;
+	nbr = 0;
+	pon = 1;
+	if (*s == '-' || *s == '+')
+		pon = (*s++ == '+') ? 1 : -1;
+	i = 0;
+	while (ft_isdigit(*s) && ++i)
+		nbr = *s++ - '0' + nbr * 10;
+	if ((i > 19 && pon == -1) || (nbr > 9223372036854775808uLL && pon == -1))
+		return (0);
+	else if ((i > 19 && pon == 1) || (nbr > 9223372036854775807uLL && pon == 1))
+		return (-1);
+	return ((int)nbr * pon);
 }
