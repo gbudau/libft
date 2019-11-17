40873994c3fbbb574f748f77359be2374e8ee56f
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-17 19:04:00 +0100

    ft_strjoin src

---
+++ srcs/ft_strjoin.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/17 17:01:46 by gbudau            #+#    #+#             */
+/*   Updated: 2019/11/17 19:02:38 by gbudau           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ret;
+	size_t	len;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(ret = malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	ft_strlcpycat(ret, s1, s2, len);
+	return (ret);
+}
