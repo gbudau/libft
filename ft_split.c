3f1827dcfe26bbec35ffd0597ae95ce0e31367f4
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-20 21:45:26 +0100

    ft_split norminette OK

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,27 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 22:19:21 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/20 18:48:51 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/11/20 21:44:40 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+static size_t	ft_wn(char const *s, char c)
+{
+	size_t i;
+
+	i = 0;
+	while (*s)
+	{
+		if ((*s != c && *(s + 1) == c) || (*s != c && !(*(s + 1))))
+			i++;
+		s++;
+	}
+	return (i);
+}
+
+char			**ft_split(char const *s, char c)
 {
 	size_t		i;
 	char const	*s_e;
@@ -21,17 +35,10 @@ char	**ft_split(char const *s, char c)
 
 	if (s == NULL)
 		return (NULL);
-	s_e = s;
-	i = 0;
-	while (*s_e)
-	{
-		if ((*s_e != c && *(s_e + 1) == c) || (*s_e != c && !(*(s_e + 1))))
-			i++;
-		s_e++;
-	}
+	i = ft_wn(s, c);
 	if (!(split = ft_calloc(i + 1, sizeof(char *))))
 		return (NULL);
-	s_e--;
+	s_e = &s[ft_strlen(s) - 1];
 	while (i && (s <= s_e))
 	{
 		if ((*s_e != c && *(s_e + 1) == c) || (*s_e != c && !(*(s_e + 1))))
