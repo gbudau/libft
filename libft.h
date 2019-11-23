a970a692b5b468f75e9b882e412d0b87ce211dd2
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-23 22:38:41 +0100

    Added ft_itoa and ft_strmapi to Makefile and libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 21:34:03 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/21 16:42:54 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/11/23 22:29:07 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strndup(const char *s1, size_t n);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
