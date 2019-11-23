2cbf4ec5dab76c5c6f3c71adc19b42986b2b905f
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-23 22:37:12 +0100

    Removed sizeof(char) from calloc since its not needed, we have count * size already

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 22:31:27 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/20 17:09:09 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/11/23 21:52:54 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	char	*ret;
 
-	if (!(ret = malloc(sizeof(char) * size * count)))
+	if (!(ret = malloc(size * count)))
 	{
 		errno = ENOMEM;
 		return (NULL);
 	}
-	ft_bzero(ret, (sizeof(char) * size * count));
+	ft_bzero(ret, (size * count));
 	return (ret);
 }
