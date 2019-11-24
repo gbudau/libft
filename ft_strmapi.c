76631825d51e571f6709d4cf9f35bdcca960c3d3
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-24 02:27:32 +0100

    Cleaned the code of ft_strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/23 22:04:18 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/23 22:35:14 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/11/23 22:46:01 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,9 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	if (!s || !(ret = ft_calloc(sizeof(char), ft_strlen(s) + 1)))
 		return (NULL);
 	i = 0;
-	while (*s != '\0')
+	while (s[i] != '\0')
 	{
-		ret[i] = (*f)(i, *s);
-		s++;
+		ret[i] = f(i, s[i]);
 		i++;
 	}
 	return (ret);
