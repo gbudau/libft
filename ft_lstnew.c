58a966a2cb73143828b1a641b54d8e52434b0257
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-28 21:00:19 +0100

    Added condition to check if content is NULL or not in ft_lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 17:23:48 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/26 18:33:58 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/11/28 20:48:47 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list *result;
+	t_list *new;
 
-	if (!(result = malloc(sizeof(t_list))))
+	if (!(new = malloc(sizeof(t_list))))
 		return (NULL);
-	result->content = content;
-	result->next = NULL;
-	return (result);
+	if (content != NULL)
+		new->content = content;
+	else
+		new->content = NULL;
+	new->next = NULL;
+	return (new);
 }
