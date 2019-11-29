c37345145ffc86ff6c0cc11085a1b8920593fa86
George Adrian Budau <gbudau@student.42madrid.com>
2019-11-29 18:31:05 +0100

    libft.h with updated prototypes of last added functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 21:34:03 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/26 20:47:25 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/11/29 17:06:40 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,7 @@ void				ft_lstadd_front(t_list **alst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list	*lst);
 void				ft_lstadd_back(t_list **alst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *))
 
 #endif
