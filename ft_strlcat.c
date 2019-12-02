e30836e6c217640ad0e4b899caf017a6d8aeb422
George Adrian Budau <gbudau@student.42madrid.com>
2019-12-02 14:47:11 +0100

    ft_strlcat dstsize 0 return len src

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 18:51:00 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/14 17:03:37 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/12/02 14:43:38 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ size_t			ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t		j;
 
 	i = 0;
+	if (dstsize == 0)
+		return (ft_strlen(src));
 	while (dst[i] && i < dstsize)
 		i++;
 	j = 0;
