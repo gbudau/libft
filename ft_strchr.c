38857bf0c9b01d96cad103e8a129727a33fea9f8
George Adrian Budau <gbudau@student.42madrid.com>
2019-12-02 14:59:18 +0100

    Casting c to const char in ft_strchr before comparing with char in string

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 15:53:48 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/19 17:50:32 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/12/02 14:49:42 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strchr(const char *s, int c)
 	p = s;
 	while (*p)
 	{
-		if (*p == c)
+		if (*p == (const char)c)
 			return ((char *)p);
 		p++;
 	}
-	if (*p == c)
+	if (*p == (const char)c)
 		return ((char *)p);
 	return (NULL);
 }
