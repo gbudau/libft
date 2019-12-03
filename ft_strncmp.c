c91dea7c568c5dc2c83e259f15a77f00789ff87d
George Adrian Budau <gbudau@student.42madrid.com>
2019-12-04 00:12:54 +0100

    ft_strncmp small change, if both strings point to the same memory adress, returns 0 without checking every byte

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 20:32:32 by gbudau            #+#    #+#             */
-/*   Updated: 2019/11/17 23:57:01 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/12/04 00:09:31 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	const char *str1;
 	const char *str2;
 
+	if (!n || s1 == s2)
+		return (0);
 	str1 = s1;
 	str2 = s2;
-	if (!n)
-		return (0);
 	while (*str1 && *str2 && (n-- - 1))
 	{
 		if (*str1 != *str2)
