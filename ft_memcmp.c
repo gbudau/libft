ca36c8aab5d974866d9a6ccec6657cca34f321bb
George Adrian Budau <gbudau@student.42madrid.com>
2019-12-04 00:11:32 +0100

    Fixed a segfault in fr_memcmp when s1 and s2 were unitialized and n = 0

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 00:43:40 by gbudau            #+#    #+#             */
-/*   Updated: 2019/12/02 12:33:38 by gbudau           ###   ########.fr       */
+/*   Updated: 2019/12/04 00:06:40 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned const char	*str1;
 	unsigned const char	*str2;
 
+	if (!n || s1 == s2)
+		return (0);
 	str1 = s1;
 	str2 = s2;
-	if (str1 == str2 || (*str1 == '\0' && *str2 == '\0'))
-		return (0);
 	while (n)
 	{
 		if (*str1 != *str2)
